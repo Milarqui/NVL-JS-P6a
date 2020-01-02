6814d1a150a06852e2268417419f6b01b824d2ba
Milarqui <59261420+Milarqui@users.noreply.github.com>
2020-01-02 11:32:05 +0100

    Create ejercicio-6a.js

---
+++ ejercicio-6a.js
@@ -0,0 +1,7 @@
+var miNombre;
+var miApellido1;
+var miApellido2;
+var miEdad;
+var diasSemana = [ "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+var g = 9.81;
+var numImpares = [ 1, 3, 5];
